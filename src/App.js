9e98d1923c55cf53bab25d2d5befc98bc2e64370
Sumita5 <satyakiroyhreeg@gmail.com>
2020-10-05 22:39:30 +0600

    client code done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import VolunteerList from './components/VolunteerList/VolunteerList';
+import AddEvent from './components/AddEvent/AddEvent';
+import Login from './components/Login/Login';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Register from './components/Register/Register';
+import RegistrationList from './components/RegistrationList/RegistrationList';
+
+export const UserContext = createContext();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loggedInUser, setLoggedInUser] = useState({});
+  
+    return (
+        <div className="App">
+            <UserContext.Provider value={[loggedInUser, setLoggedInUser]} >
+            <Router>
+              <Switch>
+                <Route path="/home"><Home /></Route>
+                <Route path="/login"><Login /></Route> 
+                  <PrivateRoute path="/register/:activityName"><Register/></PrivateRoute>
+                  <PrivateRoute path="/registrationList"><RegistrationList /></PrivateRoute>
+                <Route path="/volunteerList"><VolunteerList /></Route>
+                <Route path="/addEvent"><AddEvent /></Route>
+                <Route exact path="/"><Home /></Route>
+              </Switch>
+            </Router>
+          </UserContext.Provider>
+        </div>
+    );
 }
 
 export default App;
